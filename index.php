1c20670d4b0dec4ba5a1bd14c0ab4e0f7a8ed11f
unknown <akhil@beeone.co.uk>
2015-06-28 23:32:13 +0100

    Steffnix www init

---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name=description content="Steff &amp; Nix - Wedding Invitation">
+    <meta name=author content="">
+    <title>Steff &amp; Nix - Wedding Invitation</title>
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel=stylesheet type="text/css" media=all href="css/main_1433790530.css" />
+    <link rel=stylesheet href="http://fonts.googleapis.com/css?family=Roboto+Slab|Raleway:700|Niconne|Pacifico">
+    <script src="js/modernizr.min.js"></script>
+    <meta property="og:title" content="Steff &amp; Nix - Wedding Invitation" /> 
+    <meta property="og:image" content="http://www.steffnix.com/images/cover.jpg" /> 
+    <meta property="og:description" content="Steff &amp; Nix are getting married on 30th August 2015" /> 
+    <meta property="og:url" content="http://www.steffnix.com">
+</head>
+
+<body data-spy=scroll data-offset=0 data-target="#theMenu">
+    <nav class=menu id=theMenu>
+        <div class=menu-wrap>
+            <h3 class=logo><a href="index.php#home">Steff &amp; Nix</a></h3>
+            <a href="#home" class=smoothScroll>Home</a>
+            <a href="#story" class=smoothScroll>Our Story</a>
+            <a href="#gallery" class=smoothScroll>Gallery</a>
+            <a href="#events" class=smoothScroll>Events</a>
+            <a href="#location" class=smoothScroll>Location</a>
+            <a href="#gifts" class=smoothScroll>Join us</a>
+        </div>
+        <div id=menuToggle><i class="fa fa-heart"></i></div>
+    </nav>
+    <section id=home><h2 class="identifier">Home</h2></section>
+    <div id=headerwrap class=hero>
+        <div class=container>
+            <h1>Steff <span class=amp>&</span> Nix</h1>
+            <h2 class=amp>are getting married!</h2>
+            <h3 class=amp>on 30th August 2015</h3>
+            <div class=row>
+                <br>
+                <br>
+                <br>
+                <div class="col-lg-6 col-lg-offset-3">
+                </div>
+            </div>
+        </div>
+        <div id=overlay></div>
+        <ul id=slide-list></ul>
+        <ul id=supersized></ul>
+    </div>
+    <div class=white>
+        <div class=container>
+            <div class=row>
+                <h2>The ceremony begins in:</h2>
+                <span class=hearts></span>
+            </div>
+            <div class=row>
+                <div class="col-lg-8 col-lg-offset-2">
+                    <ul id=countdown_dashboard>
+                        <li class="dash weeks_dash">
+                            <span class=dash_title>weeks</span>
+                            <div class=digit>0</div>
+                            <div class=digit>0</div>
+                        </li>
+                        <li class="dash days_dash">
+                            <span class=dash_title>days</span>
+                            <div class=digit>0</div>
+                            <div class=digit>0</div>
+                        </li>
+                        <li class="dash hours_dash">
+                            <span class=dash_title>hours</span>
+                            <div class=digit>0</div>
+                            <div class=digit>0</div>
+                        </li>
+                        <li class="dash minutes_dash">
+                            <span class=dash_title>minutes</span>
+                            <div class=digit>0</div>
+                            <div class=digit>0</div>
+                        </li>
+                        <li class="dash seconds_dash">
+                            <span class=dash_title>seconds</span>
+                            <div class=digit>0</div>
+                            <div class=digit>0</div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section id=story><h2 class="identifier">Our Story</h2></section>
+    <div class="white nature">
+        <div class=container>
+            <div class=row>
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Our story</h2>
+                    <span class=hearts></span>
+                    <div class="row postDetails">
+                        <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2">
+                            <div class=postYear><i class="fa fa-heart"></i></div>
+                        </div>
+                        <div class="col-xs-12 col-sm-9 col-md-10 col-lg-10 rightArea">
+                            <div class=arrowpart></div>
+                            <div class=exCon>
+                                <p><span class=dropcap>P</span>eople those are meant to be together will find each other at the end.This is what we call love. When you are loved, you can do anything in creation. When you are loved, there's no need at all to understand what's happening, because everything happens within you.</p>
+                                <p><span class=dropcap>I</span>t was the pure Language of the World. It required no explanation, just as the universe needs none as it travels through endless time. What the boy felt at that moment was that he was in the presence of the only woman in his life, and that, with no need for words, she recognized the same thing. He was more certain of it than of anything in the world. He had been told by his parents and grandparents that he must fall in love and really know a person before becoming committed. But maybe people who felt that way had never learned the universal language. Because, when you know that language, it's easy to understand that someone in the world awaits you, whether it's in the middle of the desert or in some great city. And when two such people encounter each other, and their eyes meet, the past and the future become unimportant. There is only that moment, and the incredible certainty that everything under the sun has been written by one hand only. It is the hand that evokes love, and creates a twin soul for every person in the world. Without such love, one's dreams would have no meaning.
+							</div>
+                        </div>
+                    </div>
+                    <div class="row postDetails">
+                        <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2">
+                            <div class=postYear><i class="fa fa-heart"></i></div>
+                        </div>
+                        <div class="col-xs-12 col-sm-9 col-md-10 col-lg-10 rightArea">
+                            <div class=arrowpart></div>
+                            <div class=exCon>
+                                <h3>The Journey since 2003</h3>
+                                <p><span class=dropcap>W</span>e grew up in a small village over the countryside of a little town in India. Our eyes woke up early and we glared on each other at our local church for over a period. Then comes the day to exchange few words on the parish feast day in October. Thanks to Little Flower. The teenage love although quite temporary however we stayed in touch. Further the rough and soft patched enjoyed together. Ups and Downs managed by both mathematical and communicational skills. Then the genuine and real thought process over the period of late 20's to enter the world of commitment and authority. Those travel days made it simply stronger and bonded upon unconditional love and relationship. Never realised until sometime like couple of years ago that we cannot be seperated as if we do then there's no existence for us. The air we should breathe together and the journey we should carry on towards our re-birth. There we are now. Thanks to God and our loved ones for the support and the way you people received us. Now we are One and tie us with blessings so that never be two again.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section id=gallery><h2 class="identifier">Gallery</h2></section>
+    <div class=darkgrey>
+        <section id=photostack class="photostack photostack-start"><h2 class="identifier">photostack</h2>
+            <div>
+                <figure>
+                    <a href="images/1.jpg" class=photostack-img><img src="images/1s.jpg" alt=img01 /></a>
+                </figure>
+                <figure>
+                    <a href="images/2.jpg" class=photostack-img><img src="images/2s.jpg" alt=img02 /></a>
+                </figure>
+                <figure>
+                    <a href="images/3.jpg" class=photostack-img><img src="images/3s.jpg" alt=img03 /></a>
+                </figure>
+                <figure>
+                    <a href="images/4.jpg" class=photostack-img><img src="images/4s.jpg" alt=img04 /></a>
+                </figure>
+                <figure>
+                    <a href="images/5.jpg" class=photostack-img><img src="images/5s.jpg" alt=img05 /></a>
+                </figure>
+                <figure>
+                    <a href="images/6.jpg" class=photostack-img><img src="images/6s.jpg" alt=img06 /></a>
+                </figure>
+                <figure data-dummy>
+                    <a href="images/7.jpg" class=photostack-img><img src="images/7s.jpg" alt=img07 /></a>
+                </figure>
+                <figure data-dummy>
+                    <a href="images/8.jpg" class=photostack-img><img src="images/8s.jpg" alt=img08 /></a>
+                </figure>
+                <figure data-dummy>
+                    <a href="images/9.jpg" class=photostack-img><img src="images/9s.jpg" alt=img09 /></a>
+                </figure>
+                <figure data-dummy>
+                    <a href="images/10.jpg" class=photostack-img><img src="images/10s.jpg" alt=img10 /></a>
+                </figure>
+                <figure data-dummy>
+                    <a href="images/11.jpg" class=photostack-img><img src="images/11s.jpg" alt=img11 /></a>
+                </figure>
+            </div>
+        </section>
+    </div>
+    <section id=events><h2 class="identifier">Events</h2></section>
+    <div class="white noPadding">
+        <div class=container>
+            <div class=row>
+                <h2>Events</h2>
+                <span class=hearts></span>
+                <div class="col-md-6 event">
+                    <h3>Betrothal</h3>
+                    <div class=grid>
+                        <a href="javascript:;">
+                            <figure>
+                                <img src="images/event-1.jpg" alt=Betrothal />
+                            </figure>
+                        </a>
+                    </div>
+                    <div class=event-meta><span><i class="fa fa-calendar-o"></i> Aug 26, 2015</span> <span><i class="fa fa-clock-o"></i> 12:00pm</span>
+                        <span> <i class="fa fa-building-o"></i>  Little Flower Church</span></div>
+                    <br>
+                </div>
+                <div class="col-md-6  event">
+                    <h3>Wedding</h3>
+                    <div class=grid>
+                        <a href="javascript:;">
+                            <figure>
+                                <img src="images/event-2.jpg" alt=Wedding />
+                            </figure>
+                        </a>
+                    </div>
+                    <div class=event-meta><span><i class="fa fa-calendar-o"></i> Aug 30, 2015</span> <span><i class="fa fa-clock-o"></i> 4:00pm</span>
+                        <span> <i class="fa fa-building-o"></i>  Little Flower Church</span></div>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+	
+    <section id=location><h2 class="identifier">Location</h2></section>
+    <div class="white">
+        <div class=container>
+            <div class=row>
+                <div class="col-lg-12">
+					<h2>Location</h2>
+					<span class=hearts></span>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3927.0890338725108!2d76.2226269!3d10.1734183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b081a58ce3bc117%3A0xb9f1736dad49b74c!2sLittle+flower+church+Auditorium!5e0!3m2!1sen!2suk!4v1435490170969" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+            </div>
+        </div>
+    </div>
+
+    <section id=gifts><h2 class="identifier">Join us</h2></section>
+    <div class="white">
+        <div class=container>
+            <div class=row>
+                <div class="col-lg-6 col-lg-offset-3">
+                    <h2>Join the celebration</h2>
+                    <span class=hearts></span>
+                    <p class=justify><span class=dropcap>P</span>lease come and join us in our small Day. We love to have you on so that we can recollect our witnesses and well wishers before our re-birth. Let us celebrate our Love which is the universal code for all of us. Pray for our love not to fail. Love is patient, love is kind. It does not envy, it does not boast, it is not proud. It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs. Love does not delight in evil but rejoices with the truth. It always protects, always trusts, always hopes, always perseveres. Love never fails. But where there are prophecies, they will cease; where there are tongues, they will be stilled; where there is knowledge, it will pass away.</p>
+				</div>
+            </div>
+        </div>
+    </div>
+	
+    <div class="full-w">
+		<img class="inature" src="/images/green-leaf-nature-bottom.jpg" alt="Join the Celebration">
+    </div>
+	
+    <script src="js/main_1433790530.js"></script>
+
+</body>
+
+</html>
